2aa6c791e44372e2b653f977fefacf3c2d47a100
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2011-05-07 14:23:13 +0100

    Disable smartypants in the docs as it affects parsed-literal
    The result is that --option get the -- replaced.
    
    Patch provided in https://bitbucket.org/birkenfeld/sphinx/issue/647/

--- docs/conf.py
+++ docs/conf.py
@@ -130,7 +130,10 @@ html_static_path = ['_static']
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
-html_use_smartypants = True
+# Disabled because of issue #647
+# https://bitbucket.org/birkenfeld/sphinx/issue/647/
+html_use_smartypants = False
+
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
