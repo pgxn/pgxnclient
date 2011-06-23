05a6096f202758f0166d565932262621116c4856
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2011-06-23 17:23:58 +0100

    Executable scripts moved into the package directory
    It is easier to deploy, but the location can be still customized by
    packagers.

--- pgxnclient/__init__.py
+++ pgxnclient/__init__.py
@@ -8,10 +8,44 @@ pgxnclient -- main package
 
 __version__ = '0.3dev0'
 
-__all__ = ['Spec', 'SemVer', 'Label', 'Identifier']
+# Path where to find the command executables.
+# If relative, it's from the `pgxnclient` package directory.
+# Distribution packagers may move it to a different directory
+# (eventually specifying an absolute path).
+LIBEXECDIR = './libexec/'
+
+
+__all__ = [
+    'Spec', 'SemVer', 'Label', 'Identifier',
+    'get_scripts_dir', 'find_script' ]
+
+import os
 
 from pgxnclient.spec import Spec
 from pgxnclient.utils.semver import SemVer
 from pgxnclient.utils.label import Label, Identifier
+
+
+def get_scripts_dir():
+    """
+    Return the absolute path of the directory containing the client scripts.
+    """
+    return os.path.normpath(os.path.join(
+        os.path.dirname(__file__), LIBEXECDIR))
+
+def find_script(name):
+    """Return the absoulute path of a pgxn script.
+
+    The script are usually found in the `LIBEXEC` dir, but any script on the
+    path will do (they are usually prefixed by ``pgxn-``).
+
+    Return `None` if the script is not found.
+    """
+    path = os.environ.get('PATH', '').split(os.pathsep)
+    path.insert(0, get_scripts_dir())
+    for p in path:
+        fn = os.path.join(p, name)
+        if os.path.isfile(fn):
+            return fn
 
 
