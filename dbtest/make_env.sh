05e28f69af0db183a095c1f3f2f65aaaa9f8fe1b
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2011-11-12 11:06:12 +0000

    A couple of fixes to the test postgres installation script

--- dbtest/make_env.sh
+++ dbtest/make_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source env.sh
 
-wget http://wwwmaster.postgresql.org/redir/170/h/source/v${PG_VERSION}/postgresql-9.1.1.tar.bz2
+wget http://wwwmaster.postgresql.org/redir/170/h/source/v${PG_VERSION}/postgresql-${PG_VERSION}.tar.bz2
 tar xjvf postgresql-${PG_VERSION}.tar.bz2
 rm postgresql-${PG_VERSION}.tar.bz2
 cd postgresql-${PG_VERSION}
@@ -9,6 +9,6 @@ cd postgresql-${PG_VERSION}
 make
 make install
 ./root/bin/initdb -D data
-sed -e "s/#port = 5432/port = ${PG_PORT}/" postgresql-${PG_VERSION}/data/postgresql.conf \
-    | sponge postgresql-{PG_VERSION}/data/postgresql.conf
+sed -e "s/#port = 5432/port = ${PG_PORT}/" data/postgresql.conf \
+    | sponge data/postgresql.conf
 
