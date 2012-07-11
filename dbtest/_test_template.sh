5e70b4e7e2d0c653b314edeaae8c1d2d5d0a614c
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2012-07-11 10:36:46 +0100

    Fixed mess-up with db testing bin path

--- dbtest/_test_template.sh
+++ dbtest/_test_template.sh
@@ -1,16 +1,16 @@
 echo "INSTALL"
-${PGXN} install --pg_config ${PG_CONFIG} ${LEVEL} ${EXTENSION} || exit
+${PGXN} install ${LEVEL} ${EXTENSION} || exit
 
 echo "CHECK"
-${PGXN} check --pg_config ${PG_CONFIG} ${TEST_DSN} ${LEVEL} ${EXTENSION}
+${PGXN} check ${TEST_DSN} ${LEVEL} ${EXTENSION}
 
 echo "LOAD/UNLOAD"
 dropdb -h ${PG_HOST} -p ${PG_PORT} ${TEST_DB}
 createdb -h ${PG_HOST} -p ${PG_PORT} ${TEST_DB}
-${PGXN} load --pg_config ${PG_CONFIG} ${TEST_DSN} ${LEVEL} ${EXTENSION} || exit
-${PGXN} unload --pg_config ${PG_CONFIG} ${TEST_DSN} ${LEVEL} ${EXTENSION}
+${PGXN} load ${TEST_DSN} ${LEVEL} ${EXTENSION} || exit
+${PGXN} unload ${TEST_DSN} ${LEVEL} ${EXTENSION}
 
 echo "UNINSTALL"
 dropdb -h ${PG_HOST} -p ${PG_PORT} ${TEST_DB}
-${PGXN} uninstall --pg_config ${PG_CONFIG} ${LEVEL} ${EXTENSION} || exit
+${PGXN} uninstall ${LEVEL} ${EXTENSION} || exit
 
