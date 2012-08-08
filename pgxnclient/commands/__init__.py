e421474a8791459e17e6493dad2b314cebac7c19
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2012-08-08 03:36:46 +0100

    Command line argument for --pg_config uniformed to the other PROG

--- pgxnclient/commands/__init__.py
+++ pgxnclient/commands/__init__.py
@@ -448,8 +448,8 @@ class WithPgConfig(object):
         subp = super(WithPgConfig, self).customize_parser(
             parser, subparsers, **kwargs)
 
-        subp.add_argument('--pg_config', metavar="PATH", default='pg_config',
-            help = _("path to the pg_config executable to find the database"
+        subp.add_argument('--pg_config', metavar="PROG", default='pg_config',
+            help = _("the pg_config executable to find the database"
                 " [default: %(default)s]"))
 
         return subp
